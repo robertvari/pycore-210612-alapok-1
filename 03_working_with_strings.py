3301bda75b5e8c1abf8c2fd2a26fd9c1505bd560
Robert Vári <mail.robertvari@gmail.com>
2021-06-12 15:38:07 +0200

    work with strings

---
+++ 03_working_with_strings.py
@@ -0,0 +1,14 @@
+print("Robert " + "Vari")
+
+# lower() / upper()
+print("Hello World".lower())
+print("hello world".upper())
+
+# title()
+print("hello world".title())
+
+#replace()
+print("hello world".replace("l", "TTTT"))
+
+#split
+print("My name is Robert, I'm 42 years old.".split(", "))
