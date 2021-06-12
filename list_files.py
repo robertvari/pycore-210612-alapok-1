1913eca9945b21eccf814e2668330b687c215c18
Robert Vári <mail.robertvari@gmail.com>
2021-06-12 17:23:14 +0200

    lists

---
+++ list_files.py
@@ -0,0 +1,8 @@
+import os
+
+              # raw string
+folder_path = r"C:\Work\_PythonSuli\pycore-210612\alapok_01"
+
+file_list = os.listdir(folder_path)
+print(file_list)
+print( len(file_list) )
