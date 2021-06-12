c53fcabca4db854b6ae8b9f35ed2ce65c91c6a33
Robert Vári <mail.robertvari@gmail.com>
2021-06-12 17:05:55 +0200

    lists

---
+++ 07_lists.py
@@ -0,0 +1,20 @@
+my_name = "Tom"
+my_age = 34
+
+# indexes:    0    1   2   3
+my_numbers = [23, 56, 12, 46]
+
+# mixed list
+my_list = [
+    "Robert",
+    "Csaba",
+    "Christina",
+    32,
+    3.14,
+    my_name,
+    my_age,
+    my_numbers
+]
+
+print(my_list[7][-1])
+print(my_list[0])
