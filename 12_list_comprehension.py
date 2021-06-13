a1e790c66c7831b628ee9a5c80fa44ca65f6c7bd
Robert Vári <mail.robertvari@gmail.com>
2021-06-13 16:44:17 +0200

    list comprehension

---
+++ 12_list_comprehension.py
@@ -0,0 +1,11 @@
+number_list = [1, 2, 3, 4, 5]
+
+# result_list = []
+#
+# for i in number_list:
+#     result_list.append(i+100)
+
+result_list = [ i*i for i in number_list ]
+
+print(number_list)
+print(result_list)
---
+++ test.py
@@ -0,0 +1,7 @@
+number = 10
+
+if number > 10:
+    print(number)
+
+    if number < 10:
+        print(number)
