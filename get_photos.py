3e22b51a79e556b9e5a9427bfb9f52700ec9251e
Robert Vári <mail.robertvari@gmail.com>
2021-06-13 17:02:39 +0200

    get photos

--- get_photos.py
+++ get_photos.py
@@ -3,13 +3,13 @@ import os
 photo_folder = r"C:\Work\_PythonSuli\pycore-210612\photos"
 file_list = os.listdir(photo_folder)
 
-photo_list = []
+photo_list = [i for i in file_list if ".jpg" in i.lower()]
 
-for i in file_list:
-    if ".jpg" not in i.lower():
-        print(f"{i} skipped")
-        continue
-
-    photo_list.append(i)
+# for i in file_list:
+#     if ".jpg" not in i.lower():
+#         print(f"{i} skipped")
+#         continue
+#
+#     photo_list.append(i)
 
 print(photo_list)
