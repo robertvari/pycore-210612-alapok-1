540fe6a2bcf15467d92bc0af84bd6b238a637e0f
Robert Vári <mail.robertvari@gmail.com>
2021-06-13 15:03:27 +0200

    for loop

--- 10_for_loop.py
+++ 10_for_loop.py
@@ -1,7 +1,19 @@
 my_list = [1, "Robert", 3, "Kriszta", 5]
 
+# simple loop
 for item in my_list:
     print(item)
     print(f"The item value is: {item}")
+
+# exit loop
+for item in my_list:
+    if item == 3:
+        break
+
+# skip item
+for item in my_list:
+    if item == 3:
+        continue
+    print(item)
 
 print("End of code")
