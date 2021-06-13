d7e253e19850619a550a5f5fd188d0010ef118a6
Robert Vári <mail.robertvari@gmail.com>
2021-06-13 18:39:25 +0200

    update

--- collect_photo_data/main.py
+++ collect_photo_data/main.py
@@ -2,11 +2,13 @@ from PIL import Image, ExifTags
 from openpyxl import Workbook
 import os
 
-my_photo = r"C:\Work\_PythonSuli\pycore-210612\photos\IMG_1069.JPG"
-data_file = os.path.join(  os.path.dirname(my_photo), "photo_data.xlsx"  )
 
-# PIL opens photo
-img = Image.open(my_photo)
+photo_folder = r"C:\Work\_PythonSuli\pycore-210612\photos"
+file_list = os.listdir(photo_folder)
+
+photo_list = [i for i in file_list if ".jpg" in i.lower()]
+
+data_file = os.path.join(photo_folder, "photo_data.xlsx")
 
 # create workbook
 workbook = Workbook()
@@ -17,6 +19,12 @@ sheet["A1"] = "Name"
 sheet["B1"] = "Path"
 sheet["C1"] = "Size"
 
+for image_name in photo_list:
+
+    # PIL opens photo
+    img = Image.open( os.path.join(photo_folder, image_name) )
+
+
 sheet["A3"] = os.path.basename(my_photo)
 sheet["B3"] = my_photo
 sheet["C3"] = str(img.size)
@@ -24,6 +32,3 @@ sheet["C3"] = str(img.size)
 
 # save file
 workbook.save(data_file)
-
-# show image
-# img.show()
